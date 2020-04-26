53a355b148e787c835b1a86218c0cfbccc733579
ShaggyTech <beedat1@gmail.com>
2020-04-26 10:07:43 -0500

    docs(utils) - add documentation comments to utility methods

--- utils/handleError.ts
+++ utils/handleError.ts
@@ -1,4 +1,12 @@
-export const handleError = (err: Error, errMsg: string): void => {
+/**
+ * @description Will handle any errors via console.error
+ * @param {Error} err Error to handle
+ * @param {string} consoleMsg Optional prepended console message
+ * @returns void
+ */
+const handleError = (err: Error, consoleMsg: string = ''): void => {
   // eslint-disable-next-line no-console
-  console.error(`${errMsg}`, err);
+  console.error(`${consoleMsg}`, err);
 };
+
+export { handleError };
