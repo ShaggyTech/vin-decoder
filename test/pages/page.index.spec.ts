f6dda7b61135464c5229bd1d5981640e3d7091fe
ShaggyTech <beedat1@gmail.com>
2020-04-30 19:57:30 -0500

    tests(pages) - index page snapshot test

---
+++ test/pages/page.index.spec.ts
@@ -0,0 +1,35 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import Vuetify from 'vuetify';
+import vuetifyConfig from '@/plugins/vuetify';
+import pageIndex from '@/pages/index.vue';
+
+const localVue = createLocalVue();
+
+const getMocks = (history: Array<any>) => {
+  return {
+    $accessor: {
+      history: {
+        history
+      }
+    }
+  };
+};
+
+const factory = (options: object) => {
+  return mount(pageIndex, {
+    localVue,
+    ...options
+  });
+};
+
+describe('Pages: Home', () => {
+  const vuetify = new Vuetify(vuetifyConfig);
+  const wrapper = factory({
+    vuetify,
+    mocks: { ...getMocks([]) }
+  });
+
+  test('component matches snapshot', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+});
