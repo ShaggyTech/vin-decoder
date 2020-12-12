ef3c88151cc2bc1a4e24e8f743a8834b7a26db5f
Brandon Eichler <beedat1@gmail.com>
2020-12-12 14:30:04 -0600

    Merge branch 'staging' into master (#49)
    * gh-actions: rename master job
    
    * gh-actions: change deployment folder from .nuxt/dist to dist
    
    * package.json: added clean script before all production scripts
    
    * chore(config): refactor nuxt config for head name
    
    * gh-actions: remove redundant nuxt:clean run scripts
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
-import { Configuration } from '@nuxt/types';
-import { name } from './package.json';
+import { NuxtConfig } from '@nuxt/types';
+import { description, name } from './package.json';
 
+const TITLE = 'Vin Decoder';
 const isDev = process.env.NODE_ENV !== 'production';
 const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? { router: { base: `/${name}/` } }
     : {};
 
-const config: Configuration = {
+const config: NuxtConfig = {
   mode: 'universal',
   target: 'server',
   telemetry: false,
@@ -19,18 +20,25 @@ const config: Configuration = {
   privateRuntimeConfig: {},
 
   head: {
-    titleTemplate: '%s - ',
-    title: process.env.npm_package_name || '',
+    // titleTemplate: '%s - ',
+    title: TITLE,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'prefetch',
+        as: 'font',
+        href: 'https://fonts.googleapis.com/css?family=Roboto'
+      }
+    ]
   },
   loading: { color: '#fff' },
   css: [],
@@ -54,6 +62,7 @@ const config: Configuration = {
     optionsPath: '~/plugins/vuetify.ts'
   },
   build: {
+    extractCSS: !isDev,
     transpile: [
       'vee-validate/dist/rules',
       'vuetify/lib/util/colors',
