44e84573cf216a859d5f61c31d64b07b3a192e41
ShaggyTech <beedat1@gmail.com>
2020-12-13 20:06:32 -0600

    fix(tests): remove Vue.use(CompositionApi) from jest setup

--- test/config/jest.setup.js
+++ test/config/jest.setup.js
@@ -1,11 +1,9 @@
-import CompositionApi from '@nuxtjs/composition-api';
 import { config as VTUConfig } from '@vue/test-utils';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import { rules } from '@/plugins/vee-validate';
 import '@testing-library/jest-dom';
 
-Vue.use(CompositionApi);
 Vue.use(Vuetify);
 
 rules();
