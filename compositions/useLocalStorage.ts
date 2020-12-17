f61482454eee648efb6787485cce5b1868673479
ShaggyTech <beedat1@gmail.com>
2020-12-17 16:15:27 -0600

    refactor: refactor localStorage sync and use, all tests passing now

--- compositions/useLocalStorage.ts
+++ compositions/useLocalStorage.ts
@@ -5,13 +5,13 @@ import { HistoryItem } from '@/store/history';
 
 const getHistoryFromLocalStorage = (): HistoryItem[] => {
   let storage;
-  let history = [];
+  let history: HistoryItem[] = [];
   try {
     storage = localStorage.getItem('history');
     if (storage) {
       storage = JSON.parse(storage);
-      if (Array.isArray(storage?.history) && storage.history.length > 0) {
-        history = storage.history;
+      if (Array.isArray(storage) && storage.length > 0) {
+        history = [...storage];
       }
     }
     // eslint-disable-next-line no-empty
@@ -24,6 +24,6 @@ const getHistoryFromLocalStorage = (): HistoryItem[] => {
 export const syncHistoryOnMounted = (store: TypedVuexStore): void => {
   onMounted(() => {
     const localHistory = getHistoryFromLocalStorage();
-    store.history.INITIALIZE_HISTORY_STORE(localHistory);
+    store.history.INITIALIZE_HISTORY_STORE([...localHistory]);
   });
 };
--- store/plugins/syncLocalStorage.ts
+++ store/plugins/syncLocalStorage.ts
@@ -4,7 +4,7 @@ import { accessorType } from '@/store';
 export const syncLocalStorage = (store: Store<typeof accessorType>) => {
   store.subscribe((mutation, state) => {
     if (mutation.type !== 'history/INITIALIZE_HISTORY_STORE') {
-      localStorage.setItem('history', JSON.stringify(state.history));
+      localStorage.setItem('history', JSON.stringify(state.history.history));
     }
   });
 };
