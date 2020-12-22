126ca734228129c53fc11fca6f1eb7e9fc275e63
ShaggyTech <beedat1@gmail.com>
2020-12-22 01:08:11 -0600

    deployment: continuous deployment now active and working correctly on production host!

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,8 @@ wait $pid
 
 # Reload the app
 echo "New build completed, running pm2 reload"
+# I'm using nvm on my production host, so I have to specify pm2 binary path
+# to find your path, run 'pm2 startup' and use the path from the printed string with pm2 in it
 /home/shaggy/.nvm/versions/node/v14.15.3/lib/node_modules/pm2/bin/pm2 reload ecosystem.config.js --update-env
 wait
 echo "pm2 reloaded"
