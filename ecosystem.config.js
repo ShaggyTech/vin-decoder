dda2ada4ce4d5c8b73cffecf22a75c0360a6a13a
ShaggyTech <beedat1@gmail.com>
2021-06-19 14:17:19 -0500

    chore(deployment): update to node 14.17.1 - switch back to pm2 cluster mode as it works again

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,25 +4,16 @@ module.exports = {
   // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
   apps: [
     {
-      name: 'vin.dubsquared.com',
-      script: 'yarn',
-      args: 'start',
-      interpreter: '/bin/bash',
-      instances: 1, // 0 for max, specify some number for limiting
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
+      name: 'vin.shaggytech.com',
+      exec_mode: 'cluster',
+      instances: 'max',
       env: {
         NODE_ENV: 'production',
         PORT: 3000,
       },
-      // merge_logs: true,
       log_type: 'raw',
-      // script: './node_modules/@nuxt/cli-edge/bin/nuxt-cli.js',
-      // args: '["start"]',
-      // cwd: "/home/shaggy/www/vin.dubsquared.com/vin-decoder",
-      // cwd: "./",
-      // exec_mode: "cluster"
+      script: './node_modules/nuxt-edge/bin/nuxt.js',
+      args: 'start',
     },
   ],
 };
